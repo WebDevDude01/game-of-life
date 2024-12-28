c069353348ca35010334b8dda97569cc3e7d0d59
Terellc5236 <108808520+Terellc5236@users.noreply.github.com>
2024-12-28 13:23:40 -0500

    jj

---
+++ app.js
@@ -0,0 +1,17 @@
+const habitTicketForm = document.getElementById("create-habit-form");
+
+const habit = habitTicketForm.elements["habit"];//target the habit input field
+const habitLength = habitTicketForm.elements["length-of-time"];// target the habit select field
+
+
+
+
+
+
+
+
+
+
+
+
+
